b87c21caf90ca067a9d66ec75854053e87a68d99
Bhavani <bhavani@itprofound.com>
2021-02-09 18:30:56 +0530

    user leave

--- app/controllers/userLeave/createUserLeave.js
+++ app/controllers/userLeave/createUserLeave.js
@@ -1,20 +1,16 @@
-// const { createUserLeave } = require('../../services/userLeavesServices/createUserLeave');
-// const { getuserLeaveByQuery } = require('../../services/userLeavesServices/getUserLeave');
+const { createUserLeave } = require('../../services/userLeavesServices/createUserLeave');
 
-// async function createUserLeaves(req, res, next) {
-//     try {
-//         if (!findUserLeave) {
-//             const result = await createLeaveType(req.body);
-//             res.json({ status: 200, message: 'LeaveType created successfully', result: result })
-//         } else {
-//             res.json({ status: 200, message: 'LeaveType Name exist' })
-//         }
-//     } catch (error) {
-//         next(error); 
-//     }
-// }
+async function createUserLeaves(req, res, next) {
+    try {
+        const data = req.body;
+        const result = await createUserLeave(data);
+        res.json({ status: 200, message: 'Created successully!', result: result });
+    } catch (error) {
+        next(error); 
+    }
+}
 
-// module.exports = {
-//     createLeaveTypes
-// }
+module.exports = {
+    createUserLeaves
+}
 
