d771778e7d35029b9dcd0e180298d058a215401a
BhavaniAnu <email>
2021-03-02 21:14:34 +0530

    task

--- app/controllers/task/createTask.js
+++ app/controllers/task/createTask.js
@@ -1,23 +1,10 @@
 const { createTask } = require('../../services/taskServices/createTask');
-const { getTaskByQuery } = require('../../services/taskServices/getTask');
-const { updateTask } = require('../../services/taskServices/updateTask')
 
 async function createTasks(req, res, next) {
     try {
-        const findTask = await getTaskByQuery({
-            taskName: req.body.taskName
-        });
-        const data = req.body.project;
-        if (!findTask) {
-            const result = await createTask(req.body);
-            // const prjType = await updateTask({ _id: req.body.project }, 
-            //     {
-            //         $push: { projectName: data }
-            //     })
-            res.json({ status: 200, message: 'Task created successfully', result: result })
-        } else {
-            res.json({ status: 200, message: 'Task Name exist' })
-        }
+        const data = req.body;
+        const result = await createTask(data);
+        res.json({ status: 200, message: 'Task created successfully', result: result });
     } catch (error) {
         next(error);
     }
