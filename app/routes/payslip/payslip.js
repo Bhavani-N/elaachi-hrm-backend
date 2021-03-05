31afd6a917e4d756c060b8393fbf74a41fe9120b
BhavaniAnu <email>
2021-03-05 07:33:23 +0530

    upload single

--- app/routes/payslip/payslip.js
+++ app/routes/payslip/payslip.js
@@ -6,13 +6,29 @@ const { createPayslip,
     updatePayslip,
     deletePayslip } = require('../../controllers/payslip');
 
+const { uploadSingleFile } = require('../../controllers/upload');
+
 const { AuthServ } = require("../../utils/auth");
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, 'uploads')
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, `HRM_${file.originalname}`)
+    }
+}) 
+
+var upload = multer({ storage: storage })
 
 router
     .route('/')
     .post(AuthServ.authorize(), createPayslip.createPaySlips)
     .get(AuthServ.authorize(), getPayslip.getAllPayslip);
 
+router.route('/file').post(upload.single('file'), uploadSingleFile)
 
 router
     .route('/:id')
